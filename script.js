e8c6ae8795c4ac6e4b83a5a6d3f1dce96b081ce9
qwertyabu <monkey.famil4@gmail.com>
2025-05-14 21:57:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){
+    new Typed("#animated-text", {
+        strings: ["NETFLIX"],
+        typeSpeed: 100,
+        backSpeed: 99999999999999,
+        startDelay: 500,
+        loop: true
+    });
+});
